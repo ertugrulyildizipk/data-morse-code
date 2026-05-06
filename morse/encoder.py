bc3d904020f206aa09c07a3ef9044abb54f5823b
ertugrulyildizipk <ertugrul.yildiz.ipk@gmail.com>
2026-05-06 20:44:01 +0300

    Finished the encoder

--- morse/encoder.py
+++ morse/encoder.py
@@ -14,7 +14,18 @@ def encode(text):
     Encodes the given text into Morse code.
     Words are separated by a pipe (|) and letters by a space.
     """
-    pass  # YOUR CODE HERE
+    ## her kelimeyi ayrı ayrı dön
+    ## her kelimenin morse karşılığını bul çağır
+    ## kelimeleri depola sakla
+    ##sonra joinle returnle
+    words = text.split()
+    encoded_text = []
+    
+    for word in words:
+        print(word)
+        morse = encode_word(word)
+        encoded_text.append(morse)
+    return "|".join(encoded_text)
 
 
 def encode_word(word):
@@ -22,8 +33,22 @@ def encode_word(word):
     Encodes a single word into Morse code.
     Letters are separated by a space.
     """
-    pass  # YOUR CODE HERE
-
+    # her harfi morse koduna dönüştür. 
+    # sonra tek tek baralarında boşluk kullanarak birleştirip return etmem gerekir.
+    
+    ## split ile parçalara ayırabilirim -- for döngü kullanarak buna ihtiyaç kalmadan her harfte dolaşabilirim
+    ## döngü ile her parçayı dolaş
+    ## her parçanın karşılığını mors olarak bul çağır
+    ## yeni listede saklayalım
+    ## sonra da aralarında boşluk bırakarak birleştirelim
+    encoded = []
+    for char in word:
+        morse = MORSE[char.upper()]
+        encoded.append(morse)
+    
+    return " ".join(encoded)
+    
+    
 
 if __name__ == "__main__":
     # Example usage for one word
